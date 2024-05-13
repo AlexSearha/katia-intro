bd04fb9ab8e17b431f42a104cc9b2b657ada2d2a
AlexSearha <contact@mytechcompanion.fr>
2024-05-13 09:46:33 +0200

    change card text bg color

--- app/components/Cards/Text.tsx
+++ app/components/Cards/Text.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function TextElement({ text }: { text: string }) {
   return (
     <p
-      className={`${playfairDisplay.className} text-center text-3xl bg-rose-100/25 flex justify-center items-center w-full h-full`}
+      className={`${playfairDisplay.className} text-center text-3xl bg-rose-100/15 flex justify-center items-center w-full h-full`}
     >
       {text}
     </p>
